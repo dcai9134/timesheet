21866eff6e53cd3ea38c66ec5ecd5afd7dbf72ce
dcai9134 <79903898+dcai9134@users.noreply.github.com>
2021-03-02 20:12:49 +1100

    Add files via upload

---
+++ label.py
@@ -0,0 +1,20 @@
+# Database of all labelling used in timesheet application
+lb_db = {
+    'persist': {
+        'title': 'Timesheet Utility',
+        'description': ('This is a tool to assist with recording daily '
+                        'timesheets'),
+        'active': 'Active Job',
+        'timespent': 'Time Spent'
+    },
+    'tab_title': {
+        'jc': 'Job Code Settings',
+        'mon': 'Monday',
+        'tue': 'Tuesday',
+        'wed': 'Wednesday',
+        'thurs': 'Thursday',
+        'fri': 'Friday',
+        'week': 'Weekly View',
+        'graph': 'Descriptive Graphs',
+    }
+}
